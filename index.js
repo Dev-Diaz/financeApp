eecb5e814d7c0d6035e5ab5ebe734c0a2309e6d0
Felipe Diaz <felipediaz.13@icloud.com>
2024-03-19 19:24:08 -0300

    chore: add dotenv and pg setup

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-let name = ''
-let age = 0
-let teste = 5
-console.log(name, age, teste)
+import 'dotenv/config.js'
+import express from 'express'
+
+import { PostgresHelper } from './src/db/postgres/helper.js'
+
+const app = express()
+app.get('/', async (req, res) => {
+    const results = await PostgresHelper.query('SELECT * FROM users')
+    res.send(JSON.stringify(results))
+})
+
+app.listen(3000, () => {
+    console.log('Listening on por 3000')
+})
