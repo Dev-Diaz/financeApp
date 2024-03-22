6db00b8071182ca1d2f13ffa195a5d9d88a6d716
Felipe Diaz <felipediaz.13@icloud.com>
2024-03-22 12:29:41 -0300

    feat: add UpdateUserUserCase

---
+++ src/use-cases/update-user.js
@@ -0,0 +1,38 @@
+import { EmailAlreadyInUseError } from '../errors/user.js'
+import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js'
+import { PostgresUpdateUserRepository } from '../repositories/postgres/update-user'
+import bcrypt from 'bcrypt'
+
+export class UpdateUserUseCase {
+    async execute(userId, updateUserParams) {
+        if (updateUserParams.email) {
+            const postgresGetUserByEmailRepository =
+                new PostgresGetUserByEmailRepository()
+
+            const providedEmail =
+                await postgresGetUserByEmailRepository.execute(
+                    updateUserParams.email,
+                )
+            if (providedEmail) {
+                throw new EmailAlreadyInUseError(updateUserParams.email)
+            }
+        }
+
+        const user = {
+            ...updateUserParams,
+        }
+
+        if (updateUserParams.password) {
+            const hashedPassword = (updateUserParams.password =
+                await bcrypt.hash(updateUserParams.password, 10))
+            user.password = hashedPassword
+        }
+
+        const postgresUpdateUserRepository = new PostgresUpdateUserRepository()
+        const userUpdated = await postgresUpdateUserRepository.execute(
+            userId,
+            updateUserParams,
+        )
+        return userUpdated
+    }
+}
