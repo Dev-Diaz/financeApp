ce1d230674804a9510341f240de0b53a233cfd3a
Felipe Diaz <felipediaz.13@icloud.com>
2024-03-26 09:32:22 -0300

    refactor: move user use case to its own folder

--- src/factories/controllers/user.js
+++ src/factories/controllers/user.js
@@ -7,10 +7,10 @@ import { PostgresDeleteUserRepository } from '../../repositories/postgres/user/d
 import { PostgresGetUserByEmailRepository } from '../../repositories/postgres/user/get-user-by-email.js'
 import { PostgresGetUserByIdRepository } from '../../repositories/postgres/user/get-user-by-id.js'
 import { PostgresUpdateUserRepository } from '../../repositories/postgres/user/update-user.js'
-import { CreateUserUseCase } from '../../use-cases/create-user.js'
-import { DeleteUserUseCase } from '../../use-cases/delete-user.js'
-import { GetUserByIdUseCase } from '../../use-cases/get-user-by-id.js'
-import { UpdateUserUseCase } from '../../use-cases/update-user.js'
+import { CreateUserUseCase } from '../../use-cases/user/create-user.js'
+import { DeleteUserUseCase } from '../../use-cases/user/delete-user.js'
+import { GetUserByIdUseCase } from '../../use-cases/user/get-user-by-id.js'
+import { UpdateUserUseCase } from '../../use-cases/user/update-user.js'
 
 export const makeGetUserByIdController = () => {
     const getUserByidRepository = new PostgresGetUserByIdRepository()
